2699f441c815721e24daf2d6bebc582df941ecdf
Nicolas Deschamps <deschamps.nico92@gmail.com>
2022-01-23 16:00:48 +0100

    Exercice n°2 - NFT Card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def exercice1
   end
+
+  def exercice2
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'exercice1', to: 'pages#exercice1'
+  get 'exercice2', to: 'pages#exercice2'
 end
